4b689d70a952fbe114a83b35e8728e74c62d655d
undr <undr@yandex.ru>
2013-02-21 10:16:06 +0700

    Fixed handler loading

--- lib/ox/builder/handler.rb
+++ lib/ox/builder/handler.rb
@@ -1,18 +1,20 @@
-module Ox::Builder
-  class Handler
-    class_attribute :default_format
-    self.default_format = Mime::XML
+if defined?(ActionView)
+  module Ox::Builder
+    class Handler
+      class_attribute :default_format
+      self.default_format = Mime::XML
 
-    def call template
-      <<-SOURCE
-      (
-        Ox::Builder.document do |xml|
-          #{template.source}
-        end
-      ).to_s;
-      SOURCE
+      def call template
+        <<-SOURCE
+        (
+          Ox::Builder.document do |xml|
+            #{template.source}
+          end
+        ).to_s;
+        SOURCE
+      end
     end
   end
-end
 
-ActionView::Template.register_template_handler :ox, Ox::Builder::Handler.new if defined?(ActionView)
+  ActionView::Template.register_template_handler :ox, Ox::Builder::Handler.new
+end
